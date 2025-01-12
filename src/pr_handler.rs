aa56f6dab70430a98c8138d024db16c89ad528a8
Triston Armstrong <tarmstrong95@proton.me>
2025-01-12 13:12:38 -0500

    "feat(pr_handler): Add PR title and description generation rules"

--- src/pr_handler.rs
+++ src/pr_handler.rs
@@ -3,7 +3,16 @@ use crate::git_grabber::GitGrabber;
 pub struct PrHandler {}
 impl PrHandler {
     pub fn new() -> Option<(String, String)> {
-        let dirs = String::from("Pull Request Handler");
-        Some((dirs, GitGrabber::generate_repo_desc("", "")))
+        let directions = String::from("
+            <Variables>
+            change_type: feat, fix, docs, style, refactor, perf, test, build, ci, chore, revert
+            <PR Title>
+            create a short PR title from this diff, with format <change_type>(<scope>): <pr_description>. 
+            <PR Description>
+            create an additional PR Description in markdown format to describe the changes made in this diff.
+            <Response Constraints>
+            Only respond with the Pr title and Pr description. 
+        ");
+        Some((directions, GitGrabber::generate_repo_desc()))
     }
 }
