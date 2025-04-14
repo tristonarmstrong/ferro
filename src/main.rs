df01bf220a5c178001b3969463fb4c7e96df2380
Triston Armstrong <tarmstrong95@proton.me>
2025-04-13 20:41:17 -0600

    feat: Added panic handling for empty content

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod git_grabber;
 mod ml_interface;
 mod pr_handler;
 
+use core::panic;
+
 use arg_parser::ArgParser;
 use commit_handler::CommitHandler;
 use config_reader::ConfigReader;
@@ -12,6 +14,7 @@ use ml_interface::{MlBody, MlInterface, MlResponse};
 use pr_handler::PrHandler;
 
 fn main() {
+    let mut ml = MlInterface::new();
     let config = ConfigReader::new();
     let prompt: Option<(String, String)> = match ArgParser::parse() {
         Some(arg_parser::ParsedArg::Commit) => CommitHandler::new(config),
@@ -19,19 +22,24 @@ fn main() {
         None => None,
     };
 
-    // 1+2 = 5
     if prompt.is_none() {
+        println!("No prompt found!");
         return;
     }
 
-    let mut ml = MlInterface::new();
     let (directions, content) = prompt.unwrap();
+    if content.len() < 1 {
+        println!("There was no content found. Did you stage your files?");
+        return;
+    }
+
     let body = MlBody::new(content, directions);
     let res_text = ml.make_request(body).unwrap().text().unwrap();
 
     let response: Result<MlResponse, _> = serde_json::from_str(&res_text);
     if response.is_err() {
-        panic!("oop something went wrong: {:?}", response.err());
+        println!("oop something went wrong: {:?}", response.err());
+        return;
     }
     println!("{:?}", response.unwrap().response);
 }
